84bbcd695ba4d55c3c4f1cb01c5dfb38a73f9dc8
etud Yamis MANFALOTI <etud@localhost>
2024-09-24 13:01:30 +0200

    debut projet

---
+++ src/main/java/com/example/srcScanner/SourceScanner.java
@@ -0,0 +1,30 @@
+package com.example.srcScanner;
+
+public class SourceScanner {
+
+    public static void loadSourceFiles(String folderPath) {
+        // Ouvre le dossier
+
+        // Charger les règles d'inférence
+
+        // Chargle les règles d'incohérence
+
+        // Charge les faits
+
+        // Créeer la base de connaissance
+
+        // Renvoie la base de connaissance
+    }
+
+    public static void loadRulesFromSrc(String filePath) {
+        // Ouvre le fichier
+
+        // Supprimer les commentaires et les espaces de trop
+
+        // Parse les lignes
+
+        // Créer la base de règle
+
+        // Retourne la base de règle
+    }
+}
