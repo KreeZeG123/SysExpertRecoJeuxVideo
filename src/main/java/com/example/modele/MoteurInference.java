cb674d45131732952d95910de162e3aa2e2e767c
KreeZeG123 <manfaloti.yamis@gmail.com>
2024-10-01 13:30:38 +0200

    Ajout MoteurInferance.java

---
+++ src/main/java/com/example/modele/MoteurInference.java
@@ -0,0 +1,29 @@
+package com.example.modele;
+
+public class MoteurInference {
+    private BaseConnaissances BC;
+
+    public String chainageAvant() {
+        return "";
+    }
+
+    public String chainageArriere() {
+        return "";
+    }
+
+    public void modifierGroupementPaquet(Boolean etat) {
+
+    }
+
+    public void modifierChoixRegle(String choix) {
+
+    }
+
+    public void modifierModeExplication(String mode) {
+
+    }
+
+    public void chargerBC() {
+
+    }
+}
